334bb9f42c45829a6b3e38470b1ce7d8cf029626
James Ribeiro <jamesribeiro@gmail.com>
2024-09-17 17:52:19 -0300

    oauth2

--- src/main/java/br/com/paqueradebar/security/SecConfig.java
+++ src/main/java/br/com/paqueradebar/security/SecConfig.java
@@ -1,22 +1,23 @@
-//package br.com.paqueradebar.security;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-////@EnableWebSecurity
-//public class SecConfig {
-//
-//	@Bean
-//	public SecurityFilterChain secureFilterChain(HttpSecurity http) throws Exception {
-//		return http.authorizeHttpRequests(request -> {
-//			request.requestMatchers("/", "/usuario/**").permitAll();
-//			// request.anyRequest().authenticated();
-//		}).build();
-////				.oauth2Login(Customizer.withDefaults()).formLogin(Customizer.withDefaults()).build();
-////				.formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/home", true))
-////				.oauth2Login(oauth2 -> oauth2.loginPage("/login").defaultSuccessUrl("/", true))
-//	}
-//}
+package br.com.paqueradebar.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecConfig {
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+		return http.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+						.requestMatchers("/login", "/css/**", "/images/**", "/js/**").permitAll() // permite todos
+						.anyRequest().authenticated()) // so autenticado
+				.oauth2Login(oauth2 -> oauth2.loginPage("/login") // Página de login customizada (opcional)
+				).build();
+	}
+}
