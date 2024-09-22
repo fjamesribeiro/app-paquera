d68f307480bcbfcf3d8d34ab8f4f91e0db263491
fjamesribeiro <jamesribeiro@gmail.com>
2024-09-22 18:07:37 -0300

    oauth2

--- src/main/java/br/com/paqueradebar/controller/view/Home.java
+++ src/main/java/br/com/paqueradebar/controller/view/Home.java
@@ -1,21 +1,18 @@
 package br.com.paqueradebar.controller.view;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class Home {
 
-	@GetMapping("/")
-	public String home(HttpSession request) {
-		return "login";
-	}
-
-	@GetMapping("/fail")
-	public String fail() {
-		return "fail";
+	@GetMapping("/home")
+	public String home(Model model, @AuthenticationPrincipal OAuth2User auth2User) {
+		model.addAttribute("name", auth2User.getAttribute("name"));
+		return "home";
 	}
 
 }
--- src/main/java/br/com/paqueradebar/security/SecConfig.java
+++ src/main/java/br/com/paqueradebar/security/SecConfig.java
@@ -1,23 +1,22 @@
-//package br.com.paqueradebar.security;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecConfig {
-//
-//	@Bean
-//	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//
-//		return http.csrf(csrf -> csrf.disable())
-//				.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-//						.requestMatchers("/login", "/css/**", "/images/**", "/js/**", "/usuario/**").permitAll() // permite todos
-//						.anyRequest().authenticated() // so autenticado
-////				.oauth2Login(oauth2 -> oauth2.loginPage("/login") // Página de login customizada (opcional)
-//				).build();
-//	}
-//}
+package br.com.paqueradebar.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecConfig {
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+		http.authorizeHttpRequests(auth -> auth.requestMatchers("/", "/login", "/css/**", "/images/**", "/js/**")
+				.permitAll().anyRequest().authenticated())
+				.oauth2Login(oauth -> oauth.loginPage("/login").defaultSuccessUrl("/home", true))
+				.logout(logout -> logout.logoutSuccessUrl("/").permitAll());
+		return http.build();
+	}
+}
