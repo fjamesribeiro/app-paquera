d7419e720a5873c8da929190c66defeb869dca29
James Ribeiro <jamesribeiro@gmail.com>
2024-09-26 14:38:17 -0300

    primeiro teste pipeline

--- src/main/java/br/com/paqueradebar/security/SecConfig.java
+++ src/main/java/br/com/paqueradebar/security/SecConfig.java
@@ -1,22 +1,22 @@
-package br.com.paqueradebar.security;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@EnableWebSecurity
-@Configuration
-public class SecConfig {
-
-	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-		http.authorizeHttpRequests(auth -> auth.requestMatchers("/", "/login", "/css/**", "/images/**", "/js/**")
-				.permitAll().anyRequest().authenticated())
-				.oauth2Login(oauth -> oauth.loginPage("/login").defaultSuccessUrl("/home", true))
-				.logout(logout -> logout.logoutSuccessUrl("/").permitAll());
-		return http.build();
-	}
-}
+//package br.com.paqueradebar.security;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@EnableWebSecurity
+//@Configuration
+//public class SecConfig {
+//
+//	@Bean
+//	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//
+//		http.authorizeHttpRequests(auth -> auth.requestMatchers("/", "/login", "/css/**", "/images/**", "/js/**")
+//				.permitAll().anyRequest().authenticated())
+//				.oauth2Login(oauth -> oauth.loginPage("/login").defaultSuccessUrl("/home", true))
+//				.logout(logout -> logout.logoutSuccessUrl("/").permitAll());
+//		return http.build();
+//	}
+//}
